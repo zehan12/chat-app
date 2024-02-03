ec44cfbc7c9a3afff691f504395682d53bf66abc
Zehan Khan <zkhan@hootboard.com>
2024-02-04 01:31:21 +0530

    style: :lipstick: add shadcn ui

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
-      <div>
-        <h1 className="text-center text-3xl bg-red-600 text-white p-4">Chat App</h1>
+      <div className="flex flex-col justify-center items-center bg-red-700">
+        <h1 className="text-center text-3xl text-white p-4">
+          Chat App
+        </h1>
+        <Button className="w-40 bg-purple-600 hover:bg-purple-500 m-8">send</Button>{" "}
       </div>
     </>
   );
