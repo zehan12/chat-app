9996e95e376588b1e166a2ec75e0a5a4bb415a0e
Zehan Khan <zkhan@hootboard.com>
2024-02-04 01:01:59 +0530

    style: :art: tailwind css added

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,8 +2,8 @@ export function App() {
   return (
     <>
       <div>
-        <h1>Chat App</h1>
+        <h1 className="text-center text-3xl bg-red-600 text-white p-4">Chat App</h1>
       </div>
     </>
-  )
+  );
 }
