dc79275b825c5db2e4477585cb1e01b870d9178d
Zehan Khan <zkhan@hootboard.com>
2024-02-04 00:47:42 +0530

    test: :seedling: add dummy chats for api response

--- server/app.js
+++ server/app.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const chats = require("./data/dummyChat");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.status(200).json({ chats });
+});
+
+app.get("/api/chat/:chatId", (req, res) => {
+  const { chatId } = req.params;
+  const chat = chats.map((chat) => chat._id === chatId);
+  res.status(200).json({ chat });
+});
 
 module.exports = app;
