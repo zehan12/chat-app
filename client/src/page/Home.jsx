59f50878171c66b1ea2baef886da3ee38120b65e
Zehan Khan <zkhan@hootboard.com>
2024-02-04 20:39:53 +0530

    feat: :truck: update routes and create slot from auth layout

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,15 +1,8 @@
-import { Button } from "../components/ui/button";
-
 const Home = () => {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center bg-red-700">
-        <h1 className="text-center text-3xl text-white p-4">Home</h1>
-        <Button className="w-40 bg-purple-600 hover:bg-purple-500 m-8">
-          send
-        </Button>{" "}
-      </div>
-    </>
+    <div className="w-full h-screen flex justify-center">
+          <h1 className="text-center text-3xl text-white p-4">Home</h1>
+    </div>
   );
 };
 
--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,6 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "@/page/Home";
 import Chat from "@/page/Chat";
+import AuthLayout from "@/page/(auth)/AuthLayout";
+import SignIn from "@/page/(auth)/sign-in/index";
+import SignUp from "@/page/(auth)/sign-up/index";
 
 const routes = [
   {
@@ -12,6 +15,24 @@ const routes = [
     path: "/chats",
     element: <Chat />,
     errorElement: <h1>error</h1>,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "sign-out",
+        element: <></>,
+      },
+    ],
   },
 ];
 
