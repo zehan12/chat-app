e0dac7ea339e89d872fc62f0e2bd4667cd82cda8
Zehan Khan <zkhan@hootboard.com>
2024-02-04 20:35:02 +0530

    feat: :sparkles: added tailwind indicator

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,14 @@
+import { cn } from "./lib/utils";
+import { TailwindIndicator } from "./components/TailwindIndicator";
 import AllRoutes from "./routes/AllRoutes";
 
 export function App() {
   return (
     <>
-      <AllRoutes />
+      <body className={cn("min-h-screen bg-zinc-950 font-sans antialiased")}>
+        <AllRoutes />
+        <TailwindIndicator />
+      </body>
     </>
   );
 }
