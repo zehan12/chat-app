9a15ecb3cc9338194dd3b3ba6b14231f71baa89f
Zehan Khan <zkhan@hootboard.com>
2024-02-11 05:03:57 +0530

    fix: :bug: fix vite server issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  optimizeDeps: {
+    exclude: ['js-big-decimal']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
