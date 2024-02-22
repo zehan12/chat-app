93591be2da50f2f8f652ba7d347d9cf44204f59c
Zehan Khan <zkhan@hootboard.com>
2024-02-23 01:58:51 +0530

    feat: :sparkles: create view profile modal

--- client/src/components/users/ProfileModal.jsx
+++ client/src/components/users/ProfileModal.jsx
@@ -1,10 +1,39 @@
-const ProfileModal = ({ user }) => {
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+  DialogFooter,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+
+const ProfileModal = ({ user, setClose }) => {
   return (
-    <div className="w-80 h-40 bg-green-500">
-        <p>Profile modal</p>
-      <h1>{user.name}</h1>
-      <img className="w-12 h-12 rounded-full" src={user.avatar} />
-    </div>
+    <DialogContent className="sm:max-w-md">
+      <DialogHeader>
+        <DialogTitle>User Info</DialogTitle>
+        <DialogDescription>Info about the user</DialogDescription>
+      </DialogHeader>
+      <div>
+        <div className="w-full flex justify-center items-center px-5 gap-4">
+          <img className="h-40 w-40 rounded-full" alt="pp" src={user.avatar} />
+        </div>
+        <div>
+          <div>{user.name}</div>
+          <div>{user.email}</div>
+          <h3>About</h3>
+          <p>desc...</p>
+        </div>
+      </div>
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button className="w-full" type="button" variant="destructive">
+            close
+          </Button>
+        </DialogClose>{" "}
+      </DialogFooter>
+    </DialogContent>
   );
 };
 
