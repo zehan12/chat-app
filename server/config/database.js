46cb03232d801c9dd19bc2063974e32cbf45fd7f
Zehan Khan <zkhan@hootboard.com>
2024-02-25 00:45:26 +0530

    test: :test_tube: add log events in db

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const config = require("../config/config");
+const { logEvents } = require("../middlewares/logger");
 
 const connectDatabase = async () => {
-  const db = config.db.url.replace('<password>', config.db.password);
+  const db = config.db.url.replace("<password>", config.db.password);
   const options = {
     autoIndex: true,
     serverSelectionTimeoutMS: 5000,
   };
   try {
     await mongoose.connect(db, options);
-    console.log(`connected to database ✅`);
-  } catch (e) {
-    console.log(`Error connecting to mongoose due to ${e.message} ❌`);
+    console.log("connected to database ✅");
+  } catch (err) {
+    logEvents(`code: ${err.code}  message: ${err.message}`, "mongoErrLog.log");
+    console.log(`Error connecting to mongoose due to ${err.message} ❌`);
   }
 };
 
