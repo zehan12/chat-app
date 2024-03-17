69497b167ed8d66538b16ba0edd0053669aebefb
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2024-03-18 00:52:14 +0530

    feat: :christmas_tree: add app intro

--- client/src/app/app.jsx
+++ client/src/app/app.jsx
@@ -1,3 +1,18 @@
+/*-------------------------------------------------------------------
+|                    🚀 CHAT APP 🚀
+|
+|  HELLO 👋! WELCOME TO CHAT APP. THIS APP FETCHES DATA FROM AN 
+|  EXTERNAL API AND DISPLAYS THE DATA. THIS CHATTING APP ALLOWS 
+|  YOU TO COMMUNICATE WITH YOUR CONTACTS IN WEB CHAT ROOMS. IT 
+|  ENABLES YOU TO SEND AND RECEIVE MESSAGES. CHATTING APPS MAKE 
+|  IT EASIER, SIMPLER, AND FASTER TO CONNECT WITH EVERYONE AND 
+|  IT IS ALSO EASY TO USE, FEEL FREE TO REACH OUT TO ME OR OPEN 
+|  AN ISSUE ON GITHUB. THANKS!
+|
+|  🔗CREATOR: https://github.com/zehan12
+|  🔗SOURCE CODE: https://github.com/zehan12/chat-app
+|
+*-------------------------------------------------------------------*/
 import { cn } from "../lib/utils";
 import { TailwindIndicator } from "../components/TailwindIndicator";
 import AllRoutes from "../routes/AllRoutes";
